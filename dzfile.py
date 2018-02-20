fa0e55ddf73753228f412509d677ac265cf28dba
Klim <klimvitek@gmail.com>
2018-02-20 15:52:44 +0200

    Version1

---
+++ dzfile.py
@@ -0,0 +1,11 @@
+#import os
+from urllib.parse import urlparse
+def parssite(fil):
+    for i in fil:
+        z = urlparse(i)
+        l = {z.scheme, i}
+    print(l)
+    fil.close()
+
+fil = open(input("Input file name: "), 'r')
+parssite(fil)
